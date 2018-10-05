f434fbc83b372c871b28604674039c9fb3e84ae9
Joshua Gwaltney <josh.gwaltney@sony.com>
2018-10-05 11:16:43 -0700

    playing with a complete overhaul

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,24 +1,4 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Imports
 ////////////////////////////////////////////////////////////////////////////////
-import '../css/_site.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import Template from './template';
-import result from '../../games/stardew-valley/';
-
-function setBit(number, bitPosition) {
-  return number | (1 << bitPosition);
-}
-
-function getBit(number, bitPosition) {
-  return (number >> bitPosition) & 1;
-}
-
-console.log(result);
-ReactDOM.render(
-  <BrowserRouter>
-    <Template />
-  </BrowserRouter>
-  , document.getElementById('root'));
+console.log('success');
