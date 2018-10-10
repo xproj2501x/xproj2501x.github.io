520829bb3d05024f0ac32349d15b30510f45229c
Joshua Gwaltney <josh.gwaltney@sony.com>
2018-10-10 12:55:07 -0700

    Overhaul

--- src/client/js/display.js
+++ src/client/js/display.js
@@ -1,8 +1,8 @@
 /**
- * Binary Heap
+ * Display
  * ===
  *
- * @module binaryHeap
+ * @module display
  */
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -14,83 +14,94 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * BinaryHeap
+ * Display
  * @class
  */
-class BinaryHeap {
+class Display {
+
   //////////////////////////////////////////////////////////////////////////////
   // Private Properties
   //////////////////////////////////////////////////////////////////////////////
   /**
-   * The contents of the binary heap.
    * @private
-   * @type {Array}
+   * @type {HTMLElement}
    */
-  _data;
+  _container;
 
   /**
-   * The total number of nodes in the tree.
    * @private
-   * @type {number}
+   * @type {HTMLElement}
    */
-  _size;
+  _canvas;
+
+  /**
+   * @private
+   * @type {CanvasRenderingContext2D}
+   */
+  _context;
 
   //////////////////////////////////////////////////////////////////////////////
   // Public Properties
   //////////////////////////////////////////////////////////////////////////////
 
   /**
-   * BinaryHeap
+   * Display
    * @constructor
+   * @param {HTMLElement} container - The parent container for the display.
    */
-  constructor() {
-    this._data = [];
-    this._size = 0;
+  constructor(container) {
+    this._container = container;
+    this._canvas = document.createElement('canvas');
+    this._context = this._canvas.getContext('2d');
+    this._container.appendChild(this._canvas);
+    this._refresh();
   }
 
   //////////////////////////////////////////////////////////////////////////////
   // Public Methods
   //////////////////////////////////////////////////////////////////////////////
   /**
-   * Inserts a new element into the binary heap.
-   * @public
-   * @param {object} element - The element to be inserted.
+   * Draws a collection of sprites on the canvas.
+   * @param {Array} sprites - A collection of sprite objects.
    */
-  insert(element) {
-    this._data.push(element);
-    this._size++;
+  render(sprites) {
+    this._refresh();
+    this._context.save();
+
+    this._context.restore();
   }
 
-  /**
-   * Clears the binary heap.
-   * @public
-   */
-  clear() {
-    this._size = 0;
-    this._data = [];
-  }
   //////////////////////////////////////////////////////////////////////////////
   // Private Methods
   //////////////////////////////////////////////////////////////////////////////
-  _sinkDown() {
-
+  /**
+   * Clears the canvas and resets the dimensions to the current width and height of the container.
+   * @private
+   */
+  _refresh() {
+    this._context.clearRect(0, 0, this._canvas.height, this._canvas.width);
+    this._canvas.height = this._container.clientHeight;
+    this._canvas.width = this._container.clientWidth;
   }
 
   //////////////////////////////////////////////////////////////////////////////
   // Static Methods
   //////////////////////////////////////////////////////////////////////////////
   /**
-   * Static factory method.
+   * Static factory method
    * @static
+   * @param {string} containerId - The id for the container element.
    *
-   * @return {BinaryHeap} A new binary heap instance.
+   * @return {Display} A new display instance.
    */
-  static createInstance() {
-    return new BinaryHeap();
+  static createInstance(containerId) {
+    const CONTAINER = document.getElementById(containerId);
+
+    return new Display(CONTAINER);
   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Exports
 ////////////////////////////////////////////////////////////////////////////////
-export default BinaryHeap;
+export default Display;
--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,4 +1,101 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Imports
 ////////////////////////////////////////////////////////////////////////////////
-console.log('success');
+import '../css/_site.scss';
+import Display from './display';
+
+////////////////////////////////////////////////////////////////////////////////
+// Definitions
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Class
+////////////////////////////////////////////////////////////////////////////////
+class App {
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Private Properties
+  //////////////////////////////////////////////////////////////////////////////
+  /**
+   * The HTML element for the navigation menu.
+   * @private
+   * @type {HTMLElement}
+   */
+  _navMenu;
+
+  /**
+   * The HTML element for the canvas panel.
+   * @private
+   * @type {HTMLElement}
+   */
+  _canvasPanel;
+
+  /**
+   * The HTML element for the info panel.
+   * @private
+   * @type {HTMLElement}
+   */
+  _infoPanel;
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Public Properties
+  //////////////////////////////////////////////////////////////////////////////
+
+  constructor() {
+    const MENU_BUTTON = document.getElementById('menuButton');
+    const INFO_BUTTON = document.getElementById('infoButton');
+    const LINKS = document.getElementsByClassName('o-nav__menu__item');
+
+    this._navMenu = document.getElementById('navMenu');
+    this._infoPanel = document.getElementById('infoPanel');
+    MENU_BUTTON.addEventListener('click', (event) => {this.toggleMenu(event)});
+    INFO_BUTTON.addEventListener('click', (event) => {this.toggleInfoPanel(event)});
+    for (const KEY in LINKS) {
+      if (LINKS.hasOwnProperty(KEY)) {
+        LINKS[KEY].addEventListener('click', (event) => {
+          const PATH_NAME = event.target.innerText.toLowerCase();
+
+          window.history.pushState({}, PATH_NAME, window.location.origin + '/' + PATH_NAME);
+          console.log(`push: ${window.location.href}`);
+        });
+      }
+    }
+    window.onpopstate = () => {
+      console.log(`pop: ${window.location.href}`);
+    };
+
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Public Methods
+  //////////////////////////////////////////////////////////////////////////////
+  toggleMenu(event) {
+    this._navMenu.classList.toggle('o-nav--collapsed');
+  }
+
+  toggleInfoPanel(event) {
+    this._infoPanel.classList.toggle('o-info-panel--collapsed');
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Private Methods
+  //////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Static Methods
+  //////////////////////////////////////////////////////////////////////////////
+  /**
+   * Static factory method.
+   * @static
+   *
+   * @return {App} A new app instance.
+   */
+  static createInstance() {
+    return new App();
+  }
+}
+
+window.addEventListener('load', (event) => {
+  const DISPLAY = Display.createInstance('canvas-wrapper');
+  const APP = App.createInstance();
+});
